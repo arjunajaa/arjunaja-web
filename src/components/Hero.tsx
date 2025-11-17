20303201718267589194a772185169ba75209a3c
arjunajaa <arju.naja@ui.ac.id>
2025-11-17 11:47:53 +0700

    [ADD] Add LinkedIn icon link to Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -64,6 +64,13 @@ const Hero = () => {
               <a href="https://github.com/arjunajaa" target="_blank" rel="noopener noreferrer" className="group">
                 <Image src="/hero/github-icon.png" alt="GitHub" width={28} height={28} className="group-hover:opacity-75 group-hover:scale-110 transform transition-all duration-300" />
               </a>
+              <a href="https://www.linkedin.com/in/arju-naja" target="_blank" rel="noopener noreferrer" className="group">
+                <span className="inline-block align-middle">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" viewBox="0 0 24 24" className="group-hover:opacity-75 group-hover:scale-110 transform transition-all duration-300">
+                    <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.761 0 5-2.239 5-5v-14c0-2.761-2.239-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm13.5 11.268h-3v-5.604c0-1.337-.026-3.063-1.868-3.063-1.868 0-2.154 1.459-2.154 2.967v5.7h-3v-10h2.881v1.367h.041c.401-.761 1.379-1.563 2.838-1.563 3.036 0 3.6 2.001 3.6 4.601v5.595z"/>
+                  </svg>
+                </span>
+              </a>
             </div>
             <div> {/* New div for the button */}
               <Button
